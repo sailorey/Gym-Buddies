65f5460894b256eccb0a562db08dd0bbd48b547b
sailorey <reyyvit@gmail.com>
2024-06-05 02:45:39 -0500

    uploaded to render

--- gym-buddies/server.js
+++ gym-buddies/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');  // Import path module
+
 
 const userRouter = require('./routes/userRouter');
 const workoutRouter = require('./routes/workoutRouter');
@@ -23,6 +25,15 @@ app.use('/api/users', userRouter);
 app.use('/api/workouts', workoutRouter);
 app.use('/api/comments', commentRouter);
 app.use('/api/measurements', measurementRouter);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// The "catchall" handler: for any request that doesn't match one above,
+// send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 const DB_URI = process.env.DB_URI;
