d6251022842ddfad132ec6da45af7f99af12b343
sailorey <reyyvit@gmail.com>
2024-06-06 15:50:42 -0500

    Update AuthContext.js

--- gym-buddies/client/src/context/AuthContext.js
+++ gym-buddies/client/src/context/AuthContext.js
@@ -11,15 +11,27 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      const decodedToken = JSON.parse(atob(token.split('.')[1]));
-      setUser({ _id: decodedToken.id, username: decodedToken.username });
+      try {
+        const decodedToken = JSON.parse(atob(token.split('.')[1]));
+        setUser({ _id: decodedToken.id, username: decodedToken.username });
+      } catch (error) {
+        console.error('Invalid token:', error);
+        // Optionally, clear the token if it's invalid
+        localStorage.removeItem('token');
+      }
     }
   }, []);
 
   const login = (token) => {
     localStorage.setItem('token', token);
-    const decodedToken = JSON.parse(atob(token.split('.')[1]));
-    setUser({ _id: decodedToken.id, username: decodedToken.username });
+    try {
+      const decodedToken = JSON.parse(atob(token.split('.')[1]));
+      setUser({ _id: decodedToken.id, username: decodedToken.username });
+    } catch (error) {
+      console.error('Invalid token:', error);
+      // Optionally, clear the token if it's invalid
+      localStorage.removeItem('token');
+    }
   };
 
   const logout = () => {
